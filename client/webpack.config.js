994c6525589befe8a6aeabe2bbb80c1579b0dce9
Amazingandyyy <amazingandyyy@gmail.com>
2017-01-01 21:08:14 -0800

    add electron
    add electron

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,29 @@
+var webpack = require('webpack');
+var path = require('path');
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
 module.exports = {
+  externals: nodeModules,
   entry: [
     './src/index.js'
   ],
+  target: 'node',
   output: {
     path: __dirname,
     publicPath: '/',
     filename: '/bundle.js'
+  },
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, './sass')]
   },
   module: {
     loaders: [{
@@ -14,7 +32,19 @@ module.exports = {
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
-    }]
+    },
+      {
+        test: /\.sass$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.md$/,
+        loader: 'html!markdown' 
+      }]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
