def79c9b5d46a89854d48568d8478cdc7e8ed2ca
Andy Chen <amazingandyyy@gmail.com>
2018-08-08 15:43:15 -0700

    change jwt_secret to unsafe_

--- server/src/config.js
+++ server/src/config.js
@@ -6,7 +6,7 @@ if(process.env.NODE_ENV != 'production'){
 }
 
 module.exports = {
-  jwt_secret: process.env.JWT_SECRET || 'jwt_secret',
+  jwt_secret: process.env.JWT_SECRET || 'unsafe_jwt_secret',
   mongoose: {
     uri: process.env.MONGODB_URI || 'mongodb://localhost/mern-stack'
   },
