4b4727816f40238b4f9c60389b9b5a435cabf27f
Andy Chen <amazingandyyy@gmail.com>
2018-08-08 15:31:39 -0700

    update default message

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,13 +1,13 @@
-import Authentication from '../controllers/authentication';
-import Middlewares from './middlewares';
-import api from './api';
+import Authentication from '../controllers/authentication'
+import Middlewares from './middlewares'
+import api from './api'
 
-const router = require('express').Router();
+const router = require('express').Router()
 
-router.use('/api', Middlewares.loginRequired, api);
-router.post('/signup', Authentication.signup);
-router.post('/signin', Authentication.signin);
+router.use('/api', Middlewares.loginRequired, api)
+router.post('/signup', Authentication.signup)
+router.post('/signin', Authentication.signin)
 router.get('/ping', (req, res) => res.send('pong'))
-router.get('/', (req, res) => res.send('Open source here: https://github.com/amazingandyyy/mern-stack'))
+router.get('/', (req, res) => res.json({'source': 'https://github.com/amazingandyyy/mern-stack'}))
 
 export default router;
