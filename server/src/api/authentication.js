bc41b7b39425ee46b679611079ebcb40c19e661a
Andy Chen <amazingandyyy@gmail.com>
2019-07-30 12:21:25 -0700

    Update authentication.js

--- server/src/api/authentication.js
+++ server/src/api/authentication.js
@@ -69,28 +69,5 @@ export default {
                     })
                 }
             })
-    },
-
-    updateProfile: (req, res, next) => {
-        req.user.comparedPassword(req.body.password, (err, good) => {
-            if (err || !good) return res.status(401).send(err || 'Incorrect Password')
-            const userId = req.user._id;
-            const newProfile = {
-                name: {
-                    first: req.body.firstName, 
-                    last: req.body.lastName
-                }
-            };
-            delete newProfile.email;
-            delete newProfile.phone;
-            delete newProfile.password;
-            
-            UserModel.findByIdAndUpdate(userId, newProfile, {new: true})
-            .then(newUser=>{
-                res.sendStatus(200);
-            })
-            .catch(next)
-        })
     }
-    
 }
