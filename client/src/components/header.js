507b75e234ea78f1bca04ffddc0e27b34009ba65
wassef911 <1.wassef911@gmail.com>
2020-08-17 10:12:44 +0100

    structured index for better loading

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,58 +1,74 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import * as actions from '../actions';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+import { NavLink } from "react-router-dom";
 
 class Header extends Component {
-    renderSignButton(){
-        if (this.props.authenticated){
-            return (
-                <li className="nav-item">
-                    <NavLink className="nav-link" to="/signout">Sign out</NavLink>
-                </li>
-            )
-        }else{
-            return (
-                [
-                    <li className="nav-item" key="1">
-                        <NavLink to="/signin" className="nav-link">Sign in</NavLink>
-                    </li>,
-                    <li className="nav-item" key="2">
-                        <NavLink to="/signup" className="nav-link">Sign Up</NavLink>
-                    </li>
-                ]
-            )
-        }
+  renderSignButton() {
+    if (this.props.authenticated) {
+      return (
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/signout">
+            Sign out
+          </NavLink>
+        </li>
+      );
+    } else {
+      return [
+        <li className="nav-item" key="1">
+          <NavLink to="/signin" className="nav-link">
+            Sign in
+          </NavLink>
+        </li>,
+        <li className="nav-item" key="2">
+          <NavLink to="/signup" className="nav-link">
+            Sign Up
+          </NavLink>
+        </li>,
+      ];
     }
-    render() {
-        return (
-            <nav className="navbar navbar-expand-sm navbar-light bg-light">
-                <NavLink className="navbar-brand" to="/">MERN</NavLink>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/public">Public</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/account">Account</NavLink>
-                        </li>
-                    </ul>
-                    <ul className="navbar-nav">
-                        {this.renderSignButton()}
-                    </ul>
-                </div>
-            </nav>
-        )
-    }
+  }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-light bg-dark">
+        <NavLink className="navbar-brand" to="/">
+          MERN
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/public">
+                Public
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/account">
+                Account
+              </NavLink>
+            </li>
+          </ul>
+          <ul className="navbar-nav">{this.renderSignButton()}</ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
-function mapStateToProps({auth}){
-    return {
-        authenticated: auth.authenticated
-    }
+function mapStateToProps({ auth }) {
+  return {
+    authenticated: auth.authenticated,
+  };
 }
 
-export default connect(mapStateToProps, actions)(Header)
+export default connect(mapStateToProps, actions)(Header);
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-      <h1>Home</h1>
+        <h1>Home</h1>
       </div>
     );
   }
