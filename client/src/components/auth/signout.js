7b0d9479679c652ce1bcae937643ea4b78cdaef6
wassef911 <1.wassef911@gmail.com>
2020-08-17 11:32:32 +0100

    converted to func component

--- client/src/components/auth/signout.js
+++ client/src/components/auth/signout.js
@@ -1,18 +1,16 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import * as actions from '../../actions';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import * as actions from "../../actions";
 
-class Signout extends Component {
-    componentDidMount() {
-        this.props.signUserOut()
-    }
-    render() {
-        return (
-            <div>
-                <h1>Hope to see you soon!</h1>
-            </div>
-        );
-    }
+function Signout({ signUserOut }) {
+  useEffect(() => {
+    signUserOut();
+  }, []);
+  return (
+    <div>
+      <h1>Hope to see you soon!</h1>
+    </div>
+  );
 }
 
-export default connect(null, actions)(Signout)
+export default connect(null, actions)(Signout);
