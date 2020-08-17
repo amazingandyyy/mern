6843096ebebb1f695fe1f452a153fcc9a000c9f4
wassef911 <1.wassef911@gmail.com>
2020-08-17 10:51:24 +0100

    added footer

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import Header from './header';
+import React, { Component } from "react";
+import Header from "./header";
+import Footer from "./footer";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Header />
-        <div className='cointainer'>
-            {this.props.children}
-        </div>
+        <div className="cointainer">{this.props.children}</div>
+        <Footer />
       </div>
     );
   }
---
+++ client/src/components/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer class="fixed-bottom  bg-dark text-white-50">
+      <div class="text-center py-3">
+        © 2020 under MIT license{" "}
+        <a href="https://github.com/amazingandyyy/mern" target="_blank">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
