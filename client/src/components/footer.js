d5d334b34b1d85381106986aad9c5a1ab31fba6c
wassef911 <1.wassef911@gmail.com>
2020-08-17 11:25:53 +0100

    converted header to functional component and added footer icon

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer class="fixed-bottom bg-light text-black-50">
-      <div class="text-center py-3">
+    <footer className="fixed-bottom bg-light text-black-50">
+      <div className="text-center py-3">
         © 2020 under MIT license{" "}
         <a href="https://github.com/amazingandyyy/mern" target="_blank">
-          <i class="fab fa-github"></i>
+          <i className="fab fa-github"></i>
         </a>
       </div>
     </footer>
--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,11 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions";
 import { NavLink } from "react-router-dom";
 
-class Header extends Component {
-  renderSignButton() {
-    if (this.props.authenticated) {
+function Header({ authenticated }) {
+  console.log("wassef ", authenticated);
+  const renderSignButton = (auth) => {
+    if (auth)
       return (
         <li className="nav-item">
           <NavLink className="nav-link" to="/signout">
@@ -13,56 +14,55 @@ class Header extends Component {
           </NavLink>
         </li>
       );
-    } else {
-      return [
+    return (
+      <section className="d-flex">
         <li className="nav-item" key="1">
           <NavLink to="/signin" className="nav-link">
             Sign in
           </NavLink>
-        </li>,
+        </li>
         <li className="nav-item" key="2">
           <NavLink to="/signup" className="nav-link">
             Sign Up
           </NavLink>
-        </li>,
-      ];
-    }
-  }
-  render() {
-    return (
-      <nav className="navbar navbar-expand-sm bg-light text-white-50">
-        <NavLink className="navbar-brand" to="/">
-          MERN
-        </NavLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/public">
-                Public
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/account">
-                Account
-              </NavLink>
-            </li>
-          </ul>
-          <ul className="navbar-nav">{this.renderSignButton()}</ul>
-        </div>
-      </nav>
+        </li>
+      </section>
     );
-  }
+  };
+
+  return (
+    <nav className="navbar navbar-expand-sm bg-light text-white-50">
+      <NavLink className="navbar-brand logofont" to="/">
+        MERN Stack
+      </NavLink>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/public">
+              Public
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/account">
+              Account
+            </NavLink>
+          </li>
+        </ul>
+        <ul className="navbar-nav">{renderSignButton(authenticated)}</ul>
+      </div>
+    </nav>
+  );
 }
 
 function mapStateToProps({ auth }) {
