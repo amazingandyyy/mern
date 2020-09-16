dd2233513c566610e874e0c376a272a9d87a4b5a
wassef911 <1.wassef911@gmail.com>
2020-09-16 22:41:57 +0100

    added footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -5,7 +5,11 @@ function Footer() {
     <footer className="fixed-bottom bg-light text-black-50">
       <div className="text-center py-3">
         © 2020 under MIT license{" "}
-        <a href="https://github.com/amazingandyyy/mern" target="_blank">
+        <a
+          href="https://github.com/amazingandyyy/mern"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fab fa-github"></i>
         </a>
       </div>
