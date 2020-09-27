b2116bd3ddda3d0ebfd2a959e3b65651b154dca2
wassef911 <1.wassef911@gmail.com>
2020-09-27 15:27:36 +0100

    refactor style

--- client/src/components/centerCard363.js
+++ client/src/components/centerCard363.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const CenterCard363 = (props) => (
-  <div className="row">
-    <div className="col-xs-12 col-sm-2 col-md-3"></div>
-      <div className="col-sm-8 col-md-6">
-        {props.children}
-      </div>
-    <div className="col-xs-12 col-sm-2 col-md-3"></div>
+  <div
+    className="d-flex flex-row flex-nowrap align-items-center justify-content-center"
+    style={{ height: "70vh" }}
+  >
+    <div>{props.children}</div>
   </div>
-)
+);
 
 export default CenterCard363;
