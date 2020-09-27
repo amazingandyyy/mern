50af090fca4fe0aa473e89f2fcc4728e590fd2e7
wassef ben ahmed <49594956+wassef911@users.noreply.github.com>
2020-09-27 15:29:47 +0100

    Update app.js

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -7,7 +7,7 @@ export default class App extends Component {
     return (
       <div>
         <Header />
-        <div className="cointainer">{this.props.children}</div>
+        <div className="container">{this.props.children}</div>
         <Footer />
       </div>
     );
