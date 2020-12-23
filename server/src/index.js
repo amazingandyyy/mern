505550cfba03ef1919de29fd1392ce39d319e949
Andy Chen <amazingandyyy@gmail.com>
2020-12-23 11:30:06 -0800

    feat: add useUnifiedTopology

--- server/src/index.js
+++ server/src/index.js
@@ -17,7 +17,7 @@ if(!process.env.JWT_SECRET) {
 
 const app = express();
 
-mongoose.connect(config.mongoose.uri, { useNewUrlParser: true })
+mongoose.connect(config.mongoose.uri, { useNewUrlParser: true, useUnifiedTopology: true })
 .catch(err=>console.error(err));
 
 mongoose.Promise = global.Promise;
