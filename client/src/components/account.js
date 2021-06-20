8692e064b236d82f845cca83c021fcebc5f843a8
Josh Soref <jsoref@users.noreply.github.com>
2021-06-20 13:08:58 -0400

    spelling: editing
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- client/src/components/account.js
+++ client/src/components/account.js
@@ -4,7 +4,7 @@ import CenterCard363 from './centerCard363';
 import useForm from '../use-form-react';
 
 const Account = () => {
-  const [editting, setEditting] =  useState(false);
+  const [editing, setEditing] =  useState(false);
   const [errMsg, setErrMsg] =  useState('');
   const [status, setStatus] =  useState('');
   const [profile, setProfile] =  useState({});
@@ -40,22 +40,22 @@ const Account = () => {
     tryConnect();
     getUserProfile();
   }, [])
-  const switchEditting = () => {
-    setEditting(!editting)
+  const switchEditing = () => {
+    setEditing(!editing)
   }
   const cancelForm = () => {
-    setEditting(false)
+    setEditing(false)
     reset();
     getUserProfile();
   }
   const renderButtons = () => {
-    if(editting){
+    if(editing){
       return (<div className="form-group">
         <button disabled={!dirty} type="submit" className="btn-lg btn btn-light btn-block">Save Change</button>
         <button className="btn-lg btn btn-secondary btn-block" onClick={cancelForm}>Cancel</button>
       </div>)
     }else{
-      return (<button className="btn btn-light btn-lg btn-block" onClick={switchEditting}>Update Information</button>)
+      return (<button className="btn btn-light btn-lg btn-block" onClick={switchEditing}>Update Information</button>)
     }
   }
   const renderProfileForm = () => {
@@ -64,7 +64,7 @@ const Account = () => {
         <div className="form-group">
           <label>First Name:</label>
           <input
-            disabled={!editting}
+            disabled={!editing}
             type='text'
             name="firstName"
             onChange={onChange}
@@ -78,7 +78,7 @@ const Account = () => {
       <div className="form-group">
         <label>Last Name:</label>
         <input
-          disabled={!editting}
+          disabled={!editing}
           type='text'
           name="lastName"
           onChange={onChange}
