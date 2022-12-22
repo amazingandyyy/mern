8cefcf0910e4b352791292b766db4cf442cd28bf
amazingshellyyy <amazingshellyyy@gmail.com>
2022-12-21 21:21:34 -0800

    feat: change class based comp to functional comp

--- client/src/components/public.js
+++ client/src/components/public.js
@@ -1,11 +1,9 @@
-import { Component } from 'react';
-
-export default class Public extends Component {
-  render() {
-    return (
-      <div>
-      <h1>Public</h1>
-      </div>
-    );
-  }
+const Public = () => {
+  return (
+    <div>
+    <h1>Public</h1>
+    </div>
+  );
 }
+
+export default Public
